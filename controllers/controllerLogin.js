720d7c4ce22bf7788ba24efbf2f79482db376da9
Jeison Pedroso <jeison.contas@gmail.com>
2022-04-01 07:45:34 -0300

    fix login com try catch

--- controllers/controllerLogin.js
+++ controllers/controllerLogin.js
@@ -9,42 +9,49 @@ const config = require("../config/config");
 
 module.exports = {
   async login(req, res) {
-    //Recebe os params obrigatorios
-    if (req.body.password && req.body.email) {
-      var email_exist = await user.findOne({
-        where: { email: req.body.email },
-      });
-
-      if (!email_exist) {
-        res.status(404).send({
-          error: true,
-          message: config.label("email_not_found"),
+    try {
+      //Recebe os params obrigatorios
+      if (req.body.password && req.body.email) {
+        var email_exist = await user.findOne({
+          where: { email: req.body.email },
         });
+
+        if (!email_exist) {
+          res.status(404).send({
+            error: true,
+            message: config.label("email_not_found"),
+          });
+        }
+
+        //Check hash
+        const match = await bcrypt.compare(
+          req.body.password,
+          email_exist.password
+        );
+
+        //Return token jwt to use
+        if (match) {
+          const id = email_exist.id;
+          const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+            expiresIn: config.jwt_expiration, //process.env.JWT_EXPIRATION, // expires in 12hours
+          });
+
+          res.send({
+            auth: true,
+            token: token,
+          });
+        } else {
+          res.status(401).send({
+            auth: false,
+            message: config.label("password_incorrect"),
+          });
+        }
       }
-
-      //Check hash
-      const match = await bcrypt.compare(
-        req.body.password,
-        email_exist.password
-      );
-
-      //Return token jwt to use
-      if (match) {
-        const id = email_exist.id;
-        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-          expiresIn: config.jwt_expiration//process.env.JWT_EXPIRATION, // expires in 12hours
-        });
-
-        res.send({
-          auth: true,
-          token: token,
-        });
-      } else {
-        res.status(401).send({
-          auth: false,
-          message: config.label("password_incorrect"),
-        });
-      }
+    } catch (e) {
+      res.status(500).send({
+        error : true,
+        message: e.message
+      })
     }
   },
 };
